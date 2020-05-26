0e6b8d0335e047dfc3d35bbd84f25c07a14959d5
”名前” <>
2020-05-26 13:36:37 +0000

    課題の提出

--- webpack.mix.js
+++ webpack.mix.js
@@ -13,9 +13,10 @@ const mix = require('laravel-mix');
 
 mix.js('resources/js/app.js', 'public/js')
    .sass('resources/sass/app.scss', 'public/css')
-   .sass('resources/sass/admin.scss', 'public/css')
-   .scss('resources/sass/profile.scss', 'public/css');
+   .sass('resources/sass/admin.scss', 'public/css');
+//   .scss('resources/sass/profile.scss', 'public/css');
     
+   
     
     
     
