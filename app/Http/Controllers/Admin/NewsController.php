32879df33d43cdd871ae456798e85db175b963bd
”名前” <>
2020-05-31 07:03:33 +0000

    課題の提出14

--- app/Http/Controllers/Admin/NewsController.php
+++ app/Http/Controllers/Admin/NewsController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Admin;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use App\News;
 
 class NewsController extends Controller
 {
@@ -13,8 +14,30 @@ class NewsController extends Controller
         return view('admin.news.create');
     }
     
-    public function create(request $recuest)
+    public function create(Request $request)
     {
+        // Validationを行う
+        $this->validate($request, News::$rules);
+        
+        $news = new News;
+        $form = $request->all();
+        
+        // フォームから画像が送信されてきたら、保存して、$news->image_path に画像のパスを保存する
+        if (isset($form['image'])) {
+            $path = $request->file('image')->store('public/image');
+            $news->image_path = basename($path);
+        } else {
+            $news->image_path = null;
+        }
+        // フォームから送信されてきた_tokenを削除する
+        unset($form['_token']);
+        // フォームから送信されてきた_imageを削除する
+        unset($form['image']);
+        
+        // データベースに保存
+        $news->fill($form);
+        $news->save();
+        
         return redirect("admin/news/create");
     }
    
